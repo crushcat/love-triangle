199797017d5e95919efb7cef4a7793a5132c1093
crushcat <crushcat@gmail.com>
2018-04-10 16:28:14 +0300

    implemetation

--- src/index.js
+++ src/index.js
@@ -4,4 +4,18 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+  let result = 0;
+  for (let i = 0; i < preferences.length; i++) {
+    let link1 = preferences[i]-1;
+    if (link1 != i){
+    let link2 = preferences[link1]-1;
+    let link3 = preferences[link2]-1;
+    if (link3==i) {
+      result++
+    }
+    }
+    
+  }
+  return result/3;
+  
 };
